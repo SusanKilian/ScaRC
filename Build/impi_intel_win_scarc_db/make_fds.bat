638cb475c3cdd5a313a1769b157bbb15208ac393
Susanne Kilian <susanne.kilian@me.com>
2021-01-20 16:25:09 +0100

    add windows scarc makefile

--- Build/impi_intel_win_scarc_db/make_fds.bat
+++ Build/impi_intel_win_scarc_db/make_fds.bat
@@ -8,7 +8,7 @@ call ..\Scripts\setup_intel_compilers.bat
 
 Title Building FDS (Intel MPI) for 64 bit Windows
 
-make SHELL="%ComSpec%" VPATH="../../Source" -f ..\makefile impi_intel_win_64_db
+make SHELL="%ComSpec%" VPATH="../../Source" -f ..\makefile_scarc impi_intel_win_scarc_db
 if x%arg1% == xbot goto skip2
 pause
 :skip2
