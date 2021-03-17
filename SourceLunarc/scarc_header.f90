90f3792dabe21bd2db30d1bf1650510a6f5f476e
Susanne Kilian <susanne.kilian@me.com>
2021-03-17 13:55:48 +0100

    remove Lunarc sources

--- SourceLunarc/scarc_header.f90
+++
@@ -1,8 +0,0 @@
-!===========================================================================================================================
-!
-!> \brief Scalable Recursive Clustering (ScaRC): Collection of alternative pressure solvers for the FDS pressure equation
-!
-!===========================================================================================================================
-#define WITH_SCARC_AMG
-#undef WITH_SCARC_VERBOSE
-
